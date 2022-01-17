7abe3b6b714f8b8d8974279294c9919c3893f1e5
czeranox@gmail.com <czeranox@gmail.com>
2022-01-18 00:19:07 +0100

    tak1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def new
   end
 
   def create
+    respond_to do |format|
     user = User.find_by(index: params[:session][:index])
     if user && user.authenticate(params[:session][:password])
-      # Wszystko dobrze, logujemy
-      log_in user
-      redirect_to user
+      format.html {
+        log_in user
+        redirect_to user
+      }
+      format.json {
+        render json: { message: 'Poprawne dane' }
+      }
     else
-      # Niedobrze
-      render 'new'
+      format.html {
+        render 'new'
+      }
+      format.json {
+        render json: { message: 'Niepoprawne dane' }
+      }
+      end
     end
   end
 
