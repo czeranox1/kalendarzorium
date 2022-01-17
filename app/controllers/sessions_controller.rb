93b4816634513cfb95ad82786d1f2299cdb8e372
czeranox@gmail.com <czeranox@gmail.com>
2022-01-18 00:49:01 +0100

    tak2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,22 @@ class SessionsController < ApplicationController
 
   def create
     respond_to do |format|
-    user = User.find_by(index: params[:session][:index])
-    if user && user.authenticate(params[:session][:password])
-      format.html {
-        log_in user
-        redirect_to user
-      }
-      format.json {
-        render json: { message: 'Poprawne dane' }
-      }
-    else
-      format.html {
-        render 'new'
-      }
-      format.json {
-        render json: { message: 'Niepoprawne dane' }
-      }
+      user = User.find_by(index: params[:session][:index])
+      if user && user.authenticate(params[:session][:password])
+        format.html {
+          log_in user
+          redirect_to user
+        }
+        format.json {
+          render json: { token: user.token }
+        }
+      else
+        format.html {
+          render 'new'
+        }
+        format.json {
+          render json: { message: 'Niepoprawne dane' }
+        }
       end
     end
   end
