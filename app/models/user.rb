774696a175f73df0ef1885b8a1b479ead1e0a5ab
czeranox@gmail.com <czeranox@gmail.com>
2022-01-17 23:58:29 +0100

    tak

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   validates :index, presence: true, length: { is: 6 }, uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  has_secure_token
+
+  def invalidate_token
+    self.update_columns(token: nil)
+  end
 end
